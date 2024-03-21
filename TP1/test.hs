812845a904a9cc27c4a6202ac037d508ab6dbb01
MarcosPerret <mperret@udesa.edu.ar>
2024-03-21 11:54:30 -0300

    ejemplo test

--- TP1/test.hs
+++ TP1/test.hs
@@ -3,9 +3,13 @@ import Route
 import Stack
 import Vessel
 
+
+double :: Int -> Int
+double n = 2*n
+
 container1 = newC "Puerto A" 4
-container2 = newC "Puerto B" 6
-container3 = newC "Puerto C" 10
+container2 = newC "Puerto B" 8
+container3 = newC "Puerto C" 16
 
 ruta1 = newR ["Puerto A", "Puerto B", "Puerto C", "Puerto D"]
 
@@ -16,5 +20,8 @@ barco1 = newV 1 2 ruta1
 
 barco2 = loadV barco1 container1
 
+barco3 = loadV barco2 container2
 
+test = [
+    netV barco3 ==  double (netV barco2)]
 
